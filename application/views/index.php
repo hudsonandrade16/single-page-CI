6a968fe10692aab704ecb0f4b483385726e70697
Elvis Oliveira <elvis.olv@gmail.com>
2016-11-09 18:34:33 -0200

    User guide removed, views formatting

--- application/views/index.php
+++ application/views/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
     <head>
         <title>SinglePageApp</title>
@@ -44,7 +44,7 @@
                             </ul>
                         </div>
                         <div id="gallery-controller">
-                            <a href="#" id="gallery-controller-prev"><img src="assets/img/left.png" alt="Prev" /></a> 
+                            <a href="#" id="gallery-controller-prev"><img src="assets/img/left.png" alt="Prev" /></a>
                             <a href="#" id="gallery-controller-next"><img src="assets/img/right.png" alt="Next" /></a>
                         </div>
                         <div id="gallery-navigation"><ul>&nbsp;</ul></div>
@@ -53,44 +53,46 @@
                 <li id="section-two">
                     <div id="about">
                         <div class="division">
-                            <p class="titulo"> About</p>
-                            <hr>
-                            <p class="descricao"> <?php foreach($texto as $tex):
-                                                     if ($tex->tag == "About")
-                                                         print $tex->texto;
-                                                     endforeach; ?> </p></p>
+                            <p class="titulo">About</p>
+                            <hr />
+                            <p class="descricao">
+                            <?php foreach($texto as $tex): ?>
+                            <?php if ($tex->tag == "About"): ?>
+                            <?php print $tex->texto; ?>
+                            <?php endif; ?>
+                            <?php endforeach; ?>
+                            </p>
                         </div>
                         <div class="inf">
-                            
                             <ul>
-                                 <li class="protect"> 
+                                 <li class="protect">
                                     <div>
-                                        <h3 class="titulo"> Protect </h3>
-                                                <?php foreach($texto as $tex): ?>
-                                                    <?php if ($tex->tag == "Protect"): ?>
-                                                     <p class="descricao"> <?php print $tex->texto; ?> </p>
-                                                    <?php endif; ?>
-                                                <?php endforeach; ?> 
+                                      <h3 class="titulo">Protect</h3>
+                                      <?php foreach($texto as $tex): ?>
+                                      <?php if ($tex->tag == "Protect"): ?>
+                                      <p class="descricao"> <?php print $tex->texto; ?> </p>
+                                      <?php endif; ?>
+                                      <?php endforeach; ?>
                                     </div>
                                 </li>
                                 <li class="environment">
                                      <div>
-                                        <h3 class="titulo"> Environment </h3>
-                                                <?php foreach($texto as $tex): ?>
-                                                    <?php if ($tex->tag == "Environment"): ?>
-                                                         <p class="descricao"> <?php print $tex->texto; ?> </p>
-                                                    <?php endif; ?>
-                                                <?php endforeach; ?> 
+                                        <h3 class="titulo">Environment</h3>
+                                        <?php foreach($texto as $tex): ?>
+                                        <?php if ($tex->tag == "Environment"): ?>
+                                        <p class="descricao"> <?php print $tex->texto; ?> </p>
+                                        <?php endif; ?>
+                                        <?php endforeach; ?>
                                     </div>
                                 </li>
-                                <li class="family"> 
+                                <li class="family">
                                     <div>
-                                        <h3 class="titulo"> Family </h3>
-                                                <?php foreach($texto as $tex): ?>
-                                                    <?php if ($tex->tag == "Family"): ?>
-                                                        <p class="descricao"> <?php print $tex->texto; ?> </p>
-                                                    <?php endif; ?>
-                                                <?php endforeach; ?> 
+                                        <h3 class="titulo">Family</h3>
+                                        <?php foreach($texto as $tex): ?>
+                                        <?php if ($tex->tag == "Family"): ?>
+                                        <p class="descricao"> <?php print $tex->texto; ?> </p>
+                                        <?php endif; ?>
+                                        <?php endforeach; ?>
                                     </div>
                                 </li>
                             </ul>
@@ -102,40 +104,39 @@
                         <div>
                             <p class='titulo' >Contacts </p>
                             <?php if(!empty($msg)): ?>
-                             <p class='msg'>Enviado com sucesso.</p> 
+                             <p class='msg'>Enviado com sucesso.</p>
                             <?php endif; ?>
-                            
                         </div>
                         <div>
-                                <fieldset>
-                                    <div class="form">
-                                            <?php echo form_open (base_url()."Single_Page/index.php/Contacts/add_db");  ?>
-                                            <dl>
-                                                <dt><label for="name">Name</label></dt>
-                                                <dd><?php echo form_input("name", ""); ?></dd>
-                                            </dl>
-                                            <dl>
-                                                <dt><label for="email">E-mail</label></dt>
-                                                <dd><?php echo form_input("email", ""); ?></dd>
-                                            </dl>
-                                            <dl>
-                                                <dt><label for="message">Message</label></dt>
-                                                <dd><?php echo form_textarea("message", ""); ?></dd>
-                                            </dl>
-                                            <?php echo form_submit("submit", "Enviar"); ?> 
-                                            </form>
-                                    </div>
-                                    <div class="social">
-                                        <p> Redes Sociais</p>
-                                        <ul>
-                                            <li><a class="facebook" href="https://www.facebook.com/" > </a></li>
-                                            <li><a class="twitter" href="https://twitter.com/?lang=pt-br" > </a></li>
-                                            <li><a class="google-plus" href="https://plus.google.com/" > </a></li>
-                                            <li><a class="instagram" href="https://www.instagram.com/?hl=pt-br" > </a></li>
-                                            <li><a class="linkedin" href="https://www.linkedin.com/" > </a></li>
-                                        </ul>
-                                    </div>
-                                </fieldset>
+                          <fieldset>
+                              <div class="form">
+                                <?php echo form_open (base_url()."Single_Page/index.php/Contacts/add_db");  ?>
+                                  <dl>
+                                    <dt><label for="name">Name</label></dt>
+                                    <dd><?php echo form_input("name", ""); ?></dd>
+                                  </dl>
+                                  <dl>
+                                    <dt><label for="email">E-mail</label></dt>
+                                    <dd><?php echo form_input("email", ""); ?></dd>
+                                  </dl>
+                                  <dl>
+                                    <dt><label for="message">Message</label></dt>
+                                    <dd><?php echo form_textarea("message", ""); ?></dd>
+                                  </dl>
+                                  <?php echo form_submit("submit", "Enviar"); ?>
+                                </form>
+                              </div>
+                              <div class="social">
+                                  <p> Redes Sociais</p>
+                                  <ul>
+                                      <li><a class="facebook" href="https://www.facebook.com/" > </a></li>
+                                      <li><a class="twitter" href="https://twitter.com/?lang=pt-br" > </a></li>
+                                      <li><a class="google-plus" href="https://plus.google.com/" > </a></li>
+                                      <li><a class="instagram" href="https://www.instagram.com/?hl=pt-br" > </a></li>
+                                      <li><a class="linkedin" href="https://www.linkedin.com/" > </a></li>
+                                  </ul>
+                              </div>
+                          </fieldset>
                         </div>
                     </div>
                 </li>
